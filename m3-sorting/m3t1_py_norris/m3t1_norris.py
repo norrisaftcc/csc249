36ae43d312812a56d8b195c63e9e9d948e6c1c3b
teacherbot <norrisa@faytechcc.edu>
2023-02-23 18:12:25 -0500

    added debug statements tracking compares and swaps

--- m3-sorting/m3t1_py_norris/m3t1_norris.py
+++ m3-sorting/m3t1_py_norris/m3t1_norris.py
@@ -6,6 +6,8 @@ todo:
     - update repository
 """
 DEBUG = True # only print debug if True (type is bool)
+COMPARES = 0
+SWAPS = 0 
 
 def selection_sort(numbers):
    for i in range(len(numbers)-1):
@@ -14,19 +16,23 @@ def selection_sort(numbers):
       # Find index of smallest remaining element
       index_smallest = i
       for j in range(i+1, len(numbers)):
-         
+         # this if statement is a compare
+         COMPARES++
          if numbers[j] < numbers[index_smallest]:
             index_smallest = j
             if DEBUG:
                 print("\tsmallest:", numbers[index_smallest],"@",index_smallest)
       
       # Swap numbers[i] and numbers[index_smallest]
+      # obviously, this is a swap
+      SWAPS++
       temp = numbers[i]
       numbers[i] = numbers[index_smallest]
       numbers[index_smallest] = temp
       if DEBUG:
           print("\t\tswap", i, "and", index_smallest)
           print("PART_SORTED", numbers)
+          print("compares=", COMPARES, " swaps=", SWAPS)
 
 
 def main():
@@ -43,6 +49,7 @@ def main():
   print('SORTED:', numbers)
   
   #print('Total comparisons:', comparisons)
+  print("compares=", COMPARES, " swaps=", SWAPS)
 
 # start program
 if __name__ == "__main__":
