3af2232f610176d4b5840e5eb2b3e46ec1b47b44
teacherbot <norrisa@faytechcc.edu>
2023-02-28 11:26:39 -0500

    added M4T1CPP

---
+++ m4-lists/m4t1_cpp/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+/* C++ version of this:
+    # add five nodes at the head
+    head = None
+
+    for count in range(1, 6):
+        head = Node(count, head)
+    # print contents
+    probe = head
+    while probe != None:
+        print(probe.data)
+        probe = probe.next
+
+*/
+
+/*
+Purpose of M4T1:
+- Add five nodes to the head of an empty list
+- Iterate through the list, printing each node
+*/
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
