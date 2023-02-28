f8ba0167fe77302e6755fcb0814da6d1de7389af
teacherbot <norrisa@faytechcc.edu>
2023-02-28 11:21:22 -0500

    Py example of traversing nodes

--- m4-lists/m4t1/m4t1_py.py
+++ m4-lists/m4t1/m4t1_py.py
@@ -15,12 +15,24 @@ Copilot has gotten stupider -- it was of zero help here.
 Examples from  Lambert, Fundamentals of Data Structures in Python
 """
 
+
+
 class Node:
     """ Represents a singly linked node"""
+    # constructors in python use the form __init__
     def __init__(self, data=None, next=None):
         """ Instantiates a Node with None as default next """
         self.data = data
-        self.next = None
+        self.next = next
+    
+    # str method provides a string conversion (ex. for print)
+    def __str__(self):
+        #return("[data=" + self.data + " | next ]")
+        # nodes contain data and next
+        data_string = str(self.data)
+        next_string = str(self.next) 
+        return "[" + data_string + " | " + next_string + "]" 
+    
         
 class LinkedList:
     def __init__(self):
@@ -28,22 +40,35 @@ class LinkedList:
         self.head = None   # pointer to head
         self.tail = None   # pointer to tail
         
+# end of class definitions        
 def main():
-    node1 = None
-    node2 = Node("A", None)
-    node3 = Node("B", node2)
+    # add five nodes at the head
+    head = None
+    
+    for count in range(1, 6):
+        head = Node(count, head)
+    # print contents
+    probe = head
+    while probe != None:
+        print(probe.data)
+        probe = probe.next
     
     
 # run program
-#if __name__ == "__main__":
-#    main()
+if __name__ == "__main__":
+    main()
 
 # We've turned off main, so running this .py file
 # simply provides a "sandbox" of objects and methods.
 # some examples
-node1 = None # an empty link
+"""
+nullptr = None          # example of an empty link (not a node at all)
+node1 = Node(None, None) # an empty Node
 node2 = Node("A", None) # data plus an empty link
 node3 = Node("B", node2) # data plus a link to node2
+print(nullptr)
 print(node1)
 print(node2)
 print(node3)
+"""
+
