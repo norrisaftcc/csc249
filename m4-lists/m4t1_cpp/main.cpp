3c5388b5f034dbbe454145849aac9823c97c6af5
teacherbot <norrisa@faytechcc.edu>
2023-03-02 10:03:17 -0500

    Using version zy 4.6

--- m4-lists/m4t1_cpp/main.cpp
+++ m4-lists/m4t1_cpp/main.cpp
@@ -1,28 +1,45 @@
 #include <iostream>
+#include "SinglyLinkedList.h"
 using namespace std;
 
-/* C++ version of this:
-    # add five nodes at the head
-    head = None
-
-    for count in range(1, 6):
-        head = Node(count, head)
-    # print contents
-    probe = head
-    while probe != None:
-        print(probe.data)
-        probe = probe.next
-
-*/
-
 /*
-Purpose of M4T1:
-- Add five nodes to the head of an empty list
-- Iterate through the list, printing each node
+CSC 249
+M4T1 - Singly Linked Lists (cpp)
+template from zybooks 4.6
+your name
+today's date
 */
 
-int main()
-{
-    cout << "Hello world!" << endl;
-    return 0;
+int main() {
+   LinkedList numList;
+   Node* nodeA = new Node(66);
+   Node* nodeB = new Node(99);
+   Node* nodeC = new Node(44);
+   Node* nodeD = new Node(95);
+   Node* nodeE = new Node(42);
+   Node* nodeF = new Node(17);
+   ///*
+   numList.Append(nodeB);   // Add 99
+   numList.Append(nodeC);   // Add 44, make the tail
+   numList.Append(nodeE);   // Add 42, make the tail
+
+   numList.Prepend(nodeA);  // Add 66, make the head
+
+   numList.InsertAfter(nodeC, nodeD);  // Insert 95 after 44
+   numList.InsertAfter(nodeE, nodeF);  // Insert 17 after tail (42)
+
+   // Output list
+   cout << "List after adding nodes: ";
+   numList.PrintList(cout);
+
+   // Remove the tail node, then the head node
+   numList.RemoveAfter(nodeE);
+   numList.RemoveAfter(nullptr);
+
+   // Output final list
+   cout << "List after removing nodes: ";
+   numList.PrintList(cout);
+  //*/
+   
+   // LinkedList destructor frees remaining nodes
 }
